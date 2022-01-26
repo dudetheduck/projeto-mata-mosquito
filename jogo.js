ba847bed7253d6b3091d48f4a7561e450d12d65b
Dudu Santos <contato.dududev@outlook.com>
2022-01-26 16:15:11 -0300

    Update 1.2

--- jogo.js
+++ jogo.js
@@ -8,6 +8,8 @@ var criaMosquitoTempo = 1500
 var nivel = window.location.search
 nivel = nivel.replace('?', '')
 
+document.querySelectorAll('img').forEach(el => el.setAttribute('draggable', false))
+
 if (nivel === 'normal') {
     // 1500
     criaMosquitoTempo = 1500
@@ -73,6 +75,7 @@ function posicaoRandomica() {
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.draggable = 'false'
     mosquito.onclick = function() {
         this.remove()
     }
